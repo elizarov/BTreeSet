cf611890c8c74d36c622dde38562a08504b65624
Roman Elizarov <elizarov@gmail.com>
2023-07-21 15:00:02 +0200

    WTreeSet benchmark

--- benchmarks/src/BenhmarkMain.kt
+++ benchmarks/src/BenhmarkMain.kt
@@ -19,6 +19,16 @@ open class BTreeSetBenchmark : SortedSetBenchmark() {
 @Fork(1)
 @Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 8, time = 1, timeUnit = TimeUnit.SECONDS)
+open class WTreeSetBenchmark : SortedSetBenchmark() {
+    override val factory: () -> SortedSet<Key>
+        get() = {  WTreeSet(KeyComparator) }
+}
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(1)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 8, time = 1, timeUnit = TimeUnit.SECONDS)
 open class JTreeSetBenchmark : SortedSetBenchmark() {
     override val factory: () -> SortedSet<Key>
         get() = {  JTreeSet(KeyComparator) }
